65bf8489872113179f89171dba7264c99a6897dc
Alan Boris <39385685+alanb128@users.noreply.github.com>
2021-04-19 01:52:41 -0400

    Add auto detection...

--- rtsp-server/entry.sh
+++ rtsp-server/entry.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
+
 cd gst-rtsp-server/examples
-./launch --gst-debug=3 "( rpicamsrc bitrate=8000000 awb-mode=tungsten preview=false ! video/x-h264, width=640, height=480, framerate=30/1 ! h264parse ! rtph264pay name=pay0 pt=96 )"
+rpicam=$(vcgencmd get_camera)
+substring="detected=1"
+if test "${rpicam#*$substring}" != "$rpicam"
+then
+    echo "Rpi camera found, starting capture."
+    ./launch --gst-debug=3 "( rpicamsrc bitrate=8000000 awb-mode=tungsten preview=false ! video/x-h264, width=640, height=480, framerate=30/1 ! h264parse ! rtph264pay name=pay0 pt=96 )"
+else
+    echo "No Rpi camera found. Searching video devices..."
+    # Get lowest video device
+    for dev in `find /dev -iname 'video*' -printf "%f\n"`
+    do
+      a=$dev
+    done
+    echo "Starting capture using $a"
+    # put usb webcam capture here...
+fi
