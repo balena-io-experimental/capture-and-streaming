06eb3ed10202b137b1ea15745e316969a1d67179
Alan Boris <39385685+alanb128@users.noreply.github.com>
2021-04-19 01:54:13 -0400

    Create detect.sh

---
+++ rtsp-server/detect.sh
@@ -0,0 +1,8 @@
+# Finds video devices that do capture
+
+for dev in `find /dev -iname 'video*' -printf "%f\n"`
+do
+  v4l2-ctl --info --device /dev/$dev | \
+    grep -Pqzo '(?s)Device Caps.+Video Capture' && \
+    echo $dev `cat /sys/class/video4linux/$dev/name`
+done
