c0e3bbb492e4b2bba37916018e109abc0b73ab19
Alan Boris <39385685+alanb128@users.noreply.github.com>
2021-04-20 01:18:00 -0400

    Better usb camera search

--- rtsp-server/entry.sh
+++ rtsp-server/entry.sh
@@ -8,12 +8,9 @@ then
     echo "Rpi camera found, starting capture."
     ./launch --gst-debug=3 "( rpicamsrc bitrate=8000000 awb-mode=tungsten preview=false ! video/x-h264, width=640, height=480, framerate=30/1 ! h264parse ! rtph264pay name=pay0 pt=96 )"
 else
-    echo "No Rpi camera found. Searching video devices..."
-    # Get lowest video device
-    for dev in `find /dev -iname 'video*' -printf "%f\n"`
-    do
-      a=$dev
-    done
+    echo "No Rpi camera found. Searching for USB cameras..."
+    #a = list devices using v4l2 | get line below line matching *camera*(usb* | take the first line | remove leading spaces
+    a=$(v4l2-ctl --list-devices | awk '/[cC]amera.+\(usb/{getline; print}' | head -n 1 | sed -e 's/^[ \t]*//')
     echo "Starting capture using $a"
-    # put usb webcam capture here...
+    # put usb webcam capture here using device $a...
 fi
