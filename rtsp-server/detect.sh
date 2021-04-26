8a374b707db94b5c2618cfc3d257ec8704886f40
Alan Boris <39385685+alanb128@users.noreply.github.com>
2021-04-26 13:39:16 -0400

    Delete detect.sh

--- rtsp-server/detect.sh
+++
@@ -1,9 +0,0 @@
-# Finds video devices that do capture
-# Not used... yet.
-
-for dev in `find /dev -iname 'video*' -printf "%f\n"`
-do
-  v4l2-ctl --info --device /dev/$dev | \
-    grep -Pqzo '(?s)Device Caps.+Video Capture' && \
-    echo $dev `cat /sys/class/video4linux/$dev/name`
-done
