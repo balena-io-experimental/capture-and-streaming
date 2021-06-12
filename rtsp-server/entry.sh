c1918f407b9489447d5f459dcff1ea1429bbb066
Alan Boris <39385685+alanb128@users.noreply.github.com>
2021-06-12 02:36:59 -0400

    Add configuration settings

--- rtsp-server/entry.sh
+++ rtsp-server/entry.sh
@@ -5,6 +5,42 @@ red=`tput setaf 1`
 green=`tput setaf 2`
 reset=`tput sgr0`
 
+# Get Device ID if not already set
+BALENA_DEVICE_ID=$2
+if ! [ "$BALENA_DEVICE_ID" ] ; then
+	BALENA_DEVICE_ID=$(curl -sSL "$BALENA_API_URL/v3/device?\$select=id,uuid&\$filter=uuid%20eq%20'$BALENA_DEVICE_UUID'" -H "Authorization: Bearer $BALENA_API_KEY" | jq '.d[0].id')
+fi
+
+echo "Using Device ID $BALENA_DEVICE_ID"
+
+# Use Device ID to set some configurations
+
+# startX
+
+echo "Setting BALENA_HOST_CONFIG_start_x"
+
+d="{\"device\": \"$BALENA_DEVICE_ID\",\"name\": \"BALENA_HOST_CONFIG_start_x\",\"value\": \"1\"}"
+
+curl -sSL -X POST \
+"$BALENA_API_URL/v6/device_config_variable" \
+-H "Content-Type: application/json" \
+-H "Authorization: Bearer $BALENA_API_KEY" \
+--data "$d"
+
+# gpu Mem
+
+echo "Setting BALENA_HOST_CONFIG_gpu_mem"
+
+d="{\"device\": \"$BALENA_DEVICE_ID\",\"name\": \"BALENA_HOST_CONFIG_gpu_mem\",\"value\": \"192\"}"
+
+curl -sSL -X POST \
+"$BALENA_API_URL/v6/device_config_variable" \
+-H "Content-Type: application/json" \
+-H "Authorization: Bearer $BALENA_API_KEY" \
+--data "$d"
+
+# start app with proper pipeline
+
 cd gst-rtsp-server/examples
 
 if [[ -z $GST_RTSP_PIPELINE ]]
@@ -24,7 +60,7 @@ then
         v4l2-ctl --list-formats-ext
         if v4l2-ctl --list-formats-ext | grep -q "YUYV"
         then
-            if [ $R_DEVICE = "jetson-nano" ]
+            if [ "$R_DEVICE" = "jetson-nano" ]
             then
                 echo "${green}Starting capture of YUYV stream...${reset}"
                 ./launch --gst-debug=3 "( v4l2src device=$a ! nvvidconv ! video/x-raw(memory:NVMM) ! omxh264enc bitrate=6000000 control-rate=variable ! video/x-h264,profile=baseline ! rtph264pay name=pay0 pt=96 )"
@@ -33,7 +69,7 @@ then
                 ./launch --gst-debug=3 "( v4l2src device=$a !  v4l2convert ! video/x-raw,width=640,height=480,framerate=15/1 ! omxh264enc target-bitrate=6000000 control-rate=variable ! video/x-h264,profile=baseline ! rtph264pay name=pay0 pt=96 )"
             fi 
         else
-            if [ $R_DEVICE = "jetson-nano" ]
+            if [ "$R_DEVICE" = "jetson-nano" ]
             then
                 echo -e "${green}Starting capture of MJPG stream, 640x480, 10 fps...${reset}"
                 ./launch --gst-debug=3 "( v4l2src device=$a ! image/jpeg,width=640,height=480,framerate=10/1 ! queue ! jpegdec ! omxh264enc bitrate=6000000 control-rate=variable ! video/x-h264,profile=baseline ! rtph264pay name=pay0 pt=96 )"
